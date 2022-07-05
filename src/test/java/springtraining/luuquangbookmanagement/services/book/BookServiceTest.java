41088fb1958c765016895ceb09366fede7ab1596
voquangluu997 <voquangluu997@gmail.com>
2022-07-05 15:54:36 +0700

    Fix book service test

--- src/test/java/springtraining/luuquangbookmanagement/services/book/BookServiceTest.java
+++ src/test/java/springtraining/luuquangbookmanagement/services/book/BookServiceTest.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,38 +21,36 @@ import springtraining.luuquangbookmanagement.repositories.UserRepository;
 import springtraining.luuquangbookmanagement.repositories.entities.Book;
 import springtraining.luuquangbookmanagement.services.BookService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
 public class BookServiceTest {
-    @TestConfiguration
-    public static class BookTestServiceConfiguration {
-        @Bean
-        BookService bookService() {
-            return new BookService();
-        }
-    }
 
-    @MockBean
+    @Mock
     private BookRepository bookRepository;
 
-    @MockBean
+    @Mock
     private UserRepository userRepository;
 
-    @Autowired
+    @InjectMocks
     private BookService bookService;
 
-    @Before
-    public void setUp() {
-        Book book = BookMock.createBook();
-        Mockito.when(bookRepository.findById(book.getId())).thenReturn(book);
-    }
+    @Mock
+    private ModelMapper modelMapper;
+
 
     @Test
     public void getBookByIdSuccess() {
-        Book testBook = BookMock.createBook();
+        Book book = BookMock.createBook();
+        when(bookRepository.findById(book.getId())).thenReturn(book);
+        Book foundBook = bookService.getById(book.getId());
+        assertEquals(foundBook, book);
+        verify(bookRepository).findById(book.getId());
 
-        Book foundBook = bookService.getById(testBook.getId());
-        Assert.assertEquals(foundBook, testBook);
     }
 
 }
